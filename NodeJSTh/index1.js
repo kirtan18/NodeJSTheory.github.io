80a25294ca8d89fb368b8736602d677f1c5c5c00
kirtan prajapati <kirtanprajapati6602@gmail.com>
2021-05-06 14:50:23 +0530

    NodeJS

---
+++ NodeJSTh/index1.js
@@ -0,0 +1,42 @@
+// const fs = require("fs");
+
+// fs.writeFile("read.txt" , "today is awasome day" ,(err) =>{
+//    console.log("completed");
+// });
+
+const fs = require("fs");
+
+//   fs.mkdir("kirtan",(err)=>{
+//      console.log("created");
+//   });
+
+// fs.writeFile("./kirtan/bio2.txt","i love coding",(err)=>{
+//    console.log("complete");
+// });
+
+// fs.appendFile("./kirtan/bio2.txt" ," Plz love my self" ,(err)=>{
+//  console.log("done");
+// });
+
+// fs.readFile("./kirtan/bio2.txt", "utf-8", (err,data) =>{
+//    console.log(data);
+// });
+
+// fs.rename("./kirtan/bio2.txt" , "./kirtan/bio3.txt" , (err) => {
+//      console.log("complete");
+// });
+
+// fs.unlink("./kirtan/bio3.txt" , ()=>{
+//    console.log("file deleted");
+// });
+
+// fs.rmdir("./kirtan" , ()=>{
+//    console.log("Delete folder");
+// });
+
+fs.mkdir("StringModule" , (err) =>{
+   console.log("Created");
+});
+
+
+ 
---
+++ NodeJSTh/osModule/os.js
@@ -0,0 +1,18 @@
+
+const os = require("os");
+
+console.log(os.arch());
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.tmpdir());
+console.log(os.type());
+
+
+// const freeMem = os.freemem();
+// // console.log(freeMem);
+// console.log(`${freeMem / 1024 / 1024 / 1024}`);
+
+
+// const totalMem = os.totalmem();
+// // console.log(freeMem);
+// console.log(`${totalMem / 1024 / 1024 / 1024}`);
